6dddacb5972fa39307c3e0103f2955cf612e44fd
Illya Havsiyevych <illya.havsiyevych@gmail.com>
2018-02-27 15:03:38 +0200

    RSI and MACD added

--- extensions/strategies/trend_bollinger/strategy.js
+++ extensions/strategies/trend_bollinger/strategy.js
@@ -1,6 +1,8 @@
 let z = require('zero-fill')
   , n = require('numbro')
   , bollinger = require('../../../lib/bollinger')
+  , rsi = require('../../../lib/rsi')
+  , ema = require('../../../lib/ema')
 
 
 function getUpperBound(s) {
@@ -56,6 +58,29 @@ function getBBColor(s, upperBound, lowerBound) {
   }
 }
 
+function macd(s) {
+  ema(s, 'ema_short', s.options.ema_short_period)
+  ema(s, 'ema_long', s.options.ema_long_period)
+  if (s.period.ema_short && s.period.ema_long) {
+    s.period.macd = (s.period.ema_short - s.period.ema_long)
+    ema(s, 'signal', s.options.signal_period, 'macd')
+    if (s.period.signal) {
+      s.period.macd_histogram = s.period.macd - s.period.signal
+    }
+  }
+}
+
+function getMACDColor(s) {
+  if (s.period.macd_histogram > 0) {
+    return 'green'
+  }
+  else if (s.period.macd_histogram < 0) {
+    return 'red'
+  } else {
+    return 'grey'
+  }
+}
+
 
 module.exports = {
   name: 'trend_bollinger',
@@ -65,15 +90,24 @@ module.exports = {
     this.option('period', 'period length, same as --period_length', String, '1h')
     this.option('period_length', 'period length, same as --period', String, '1h')
     this.option('min_periods', 'min. number of history periods', Number, 52)
+
     this.option('bollinger_size', 'period size', Number, 20)
     this.option('bollinger_time', 'times of standard deviation between the upper band and the moving averages', Number, 2)
     this.option('bollinger_upper_bound_pct', 'pct the current price should be near the bollinger upper bound before we sell', Number, 0)
     this.option('bollinger_lower_bound_pct', 'pct the current price should be near the bollinger lower bound before we buy', Number, 0)
     this.option('bollinger_width_threshold', 'bollinger width threshold', Number, 0.07)
+
+    this.option('rsi_periods', 'number of RSI periods', 14)
+
+    this.option('ema_short_period', 'number of periods for the shorter EMA', Number, 12)
+    this.option('ema_long_period', 'number of periods for the longer EMA', Number, 26)
+    this.option('signal_period', 'number of periods for the signal EMA', Number, 9)
   },
 
   calculate: function (s) {
     bollinger(s, 'bollinger', s.options.bollinger_size)
+    rsi(s, 'rsi', s.options.rsi_periods)
+    macd(s)
   },
 
   onPeriod: function (s, cb) {
@@ -111,13 +145,19 @@ module.exports = {
 
   onReport: function (s) {
     let cols = []
+
+    if (typeof s.period.macd_histogram === 'number') {
+      let color = getMACDColor(s)
+      cols.push(z(9, n(s.period.macd_histogram).format('+0.00000'), ' ')[color])
+    }
+
     if (s.period.bollinger && s.period.bollinger.upper && s.period.bollinger.lower) {
       let upperBound = getUpperBound(s)
       let lowerBound = getLowerBound(s)
       let bbw = getBBW(s, upperBound, lowerBound)
 
       let color = getBBWColor(s, bbw)
-      cols.push(z(5, n(bbw).format('0.000').substring(0,9), ' ')[color])
+      cols.push(z(6, n(bbw).format('0.000').substring(0,5), ' ')[color])
 
       color = getBBColor(s, upperBound, lowerBound)
       cols.push(z(10, n(lowerBound).format('0.00000000').substring(0,9), ' ')[color])
